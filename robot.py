2494131c9dacb1f24ba95af3aecb15b9ba57e686
kellyfreet13 <kelly.freet13@yahoo.com>
2019-02-02 22:56:50 -0800

    basic impl with backtracking sadly. added a 'pos' class attr

--- robot.py
+++ robot.py
@@ -11,51 +11,62 @@ class Robot:
         # current location
         self.loc = self.start
 
-        print(self.start)
-
-        # last path will be straight up/down column 18
-        self.middle_track = [1, 4, 7, 10, 13, 16]
+        self.pos = 'left'
 
         # the map of dirty locations
         self.map = None
-
-    def is_centered(self):
-        return self.loc.col in self.middle_track
 
     def clean(self, task: Map):
         # set the map as a class variable
         self.map = task
 
-        # check if a side is dirty
-        if self.map.is_dirty(self.loc.mid_left()):
-            self.loc = self.loc.mid_left()   # move left
-            self.map.clean(self.loc)         # and clean
-        elif self.map.is_dirty(self.loc.mid_right()):
-            self.loc = self.loc.mid_right()  # move right
-            self.map.clean(self.loc)         # and clean
+        # just for testing, you'll get an out of bounds exception
+        # if you're not stepping through the debugger
+        i = 0
+        while i < 10:
+            print('Robot at: ', self.loc)
+            if self.map.is_dirty(self.loc):
+                self.map.clean(self.loc)
 
-        # special case
-        #   this way we won't have to do unnecessary backtracking in the future
-        # - 0 -
-        # 1 0 1
-        if (
-            self.is_centered() and
-            self.map.is_dirty(self.loc.vert_left()) and
-            self.map.is_dirty(self.loc.vert_right())
-        ):
-            # move lower left and clean
-            self.loc = self.loc.vert_left()
-            self.map.clean(self.loc)
+            if not self.loc.is_centered():
+                if self.pos == 'left':
+                    self.loc = self.loc.mid_right()
+                    continue
+                elif self.pos == 'right':
+                    self.loc = self.loc.mid_left()
+                    continue
 
-            # move two right (old lower right perspective)
-            self.loc = self.loc.mid_right()
-            self.loc = self.loc.mid_right()
-            self.map.clean(self.loc)  # now clean it
+            # check if a side is dirty
+            if self.map.is_dirty(self.loc.mid_left()):
+                self.loc = self.loc.mid_left()   # move left
+                self.map.clean(self.loc)         # and clean
+                self.pos = 'left'
+            elif self.map.is_dirty(self.loc.mid_right()):
+                self.loc = self.loc.mid_right()  # move right
+                self.map.clean(self.loc)         # and clean
+                self.pos = 'right'
 
+            # left diagonal is dirty
+            elif self.map.is_dirty(self.loc.vert_left()):
+                self.loc = self.loc.vert_left()
+                self.map.clean(self.loc)
 
+                self.loc = self.loc.mid_right()  # return to center
 
+            # right diagonal is dirty
+            elif self.map.is_dirty(self.loc.vert_right()):
+                self.loc = self.loc.vert_right()
+                self.map.clean(self.loc)
 
+                self.loc = self.loc.mid_left()  # return to center
 
+            # center is dirty, and diagonals aren't
+            elif self.map.is_dirty(self.loc.vert_center()):
+                self.loc = self.loc.vert_center()
+                self.map.clean(self.loc)
+
+            self.map.show()
+            i += 1
 
     def show(self):
         print('Number of steps: ', len(self.track) - 1)
@@ -63,6 +74,14 @@ class Robot:
 
 if __name__ == '__main__':
     home = Map(19, 19)
+
+    test_map = [
+        [0, 0, 1],
+        [1, 0, 1],
+        [1, 0, 0]
+    ]
+    home.set_map(test_map)
+
     home.show()
     agent = Robot()
     agent.clean(home)
