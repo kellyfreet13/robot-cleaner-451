b3a234aa72bfda478985af298de43a87b071933f
kellyfreet13 <kelly.freet13@yahoo.com>
2019-02-04 14:05:51 -0800

    added tracking and corrected direction change w/new impl. of change_vert now in robot.py

--- robot.py
+++ robot.py
@@ -21,56 +21,68 @@ class Robot:
         # set the map as a class variable
         self.map = task
 
-        # just for testing, you'll get an out of bounds exception
-        # if you're not stepping through the debugger
+        # just for testing, it will just move back and forth at the end
         i = 0
         while i < 10:
-            print('Robot at: ', self.loc)
+            self.map.show()
+            print('Robot at: ', self.loc, '\n')
             if self.map.is_dirty(self.loc):
                 self.map.clean(self.loc)
 
             if not self.loc.is_centered():
                 if self.pos == 'left':
                     self.loc = self.loc.mid_right()
+                    self.track.append(self.loc)
                     continue
                 elif self.pos == 'right':
                     self.loc = self.loc.mid_left()
+                    self.track.append(self.loc)
                     continue
 
             # check if a side is dirty
             if self.map.is_dirty(self.loc.mid_left()):
                 self.loc = self.loc.mid_left()   # move left
                 self.map.clean(self.loc)         # and clean
+                self.track.append(self.loc)      # update track
                 self.pos = 'left'
             elif self.map.is_dirty(self.loc.mid_right()):
                 self.loc = self.loc.mid_right()  # move right
                 self.map.clean(self.loc)         # and clean
+                self.track.append(self.loc)      # update track
                 self.pos = 'right'
 
             # left diagonal is dirty
-            elif self.map.is_dirty(self.loc.vert_left()):
-                self.loc = self.loc.vert_left()
+            elif self.map.is_dirty(self.loc.vert_left(self.vert_dir)):
+                self.loc = self.loc.vert_left(self.vert_dir)
+                self.track.append(self.loc)
                 self.map.clean(self.loc)
 
                 self.loc = self.loc.mid_right()  # return to center
+                self.track.append(self.loc)
 
             # right diagonal is dirty
-            elif self.map.is_dirty(self.loc.vert_right()):
-                self.loc = self.loc.vert_right()
+            elif self.map.is_dirty(self.loc.vert_right(self.vert_dir)):
+                self.loc = self.loc.vert_right(self.vert_dir)
+                self.track.append(self.loc)
                 self.map.clean(self.loc)
 
                 self.loc = self.loc.mid_left()  # return to center
+                self.track.append(self.loc)
 
             # center is dirty, and diagonals aren't
-            elif self.map.is_dirty(self.loc.vert_center()):
-                self.loc = self.loc.vert_center()
+            elif self.map.is_dirty(self.loc.vert_center(self.vert_dir)):
+                self.loc = self.loc.vert_center(self.vert_dir)
                 self.map.clean(self.loc)
+                self.track.append(self.loc)
 
-            self.map.show()
             i += 1
+            print('Robot has made %d moves' % len(self.track))
 
     def show(self):
         print('Number of steps: ', len(self.track) - 1)
+
+        # requirement of the assignment
+        assert len(self.track) < self.map.get_map_size()
 
 
 if __name__ == '__main__':
@@ -82,8 +94,7 @@ if __name__ == '__main__':
         [1, 0, 0]
     ]
     home.set_map(test_map)
-
-    home.show()
+    #home.show()
     agent = Robot()
     agent.clean(home)
     agent.show()
